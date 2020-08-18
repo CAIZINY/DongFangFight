d7b6da4c0a09c833f47d83565504bd5409e04b1f
CAIZINY <60028330+CAIZINY@users.noreply.github.com>
2020-08-18 11:24:46 +0800

    Update AirborneJumpTypeProcess.cs
    template of state process

--- Fight/Assets/AirborneJumpTypeProcess.cs
+++ Fight/Assets/AirborneJumpTypeProcess.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AirborneJumpTypeProcess : StateMachineBehaviour
 {
+    readonly int m_HashParamsAirborneJumpType = Animator.StringToHash("AirborneJumpType");
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
@@ -17,10 +18,16 @@ public class AirborneJumpTypeProcess : StateMachineBehaviour
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        switch (stateInfo.shortNameHash)
+        {
+            case m_Hash :
+                
+                animator.SetInteger(m_HashParamsAirborneJumpType,);
+                break;
+        }
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
